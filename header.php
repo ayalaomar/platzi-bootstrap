4ee91ed3efd9eb0a50a5f75480bca87d897c2e39
Omar Ayala <ayalaa.omar@gmail.com>
2022-09-14 17:57:18 -0700

    first comit

---
+++ header.php
@@ -0,0 +1,30 @@
+<nav id="header" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+     <div class="container">
+        <a class="navbar-brand d-flex" href="#">
+            <img src="assets/images/platzi-logo.png" alt="Platzi logo">
+             &nbsp;Conf Hawaii    
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#main">La Conferencia</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#speakers">Los Oradores</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#place-time">El lugar y fecha</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#conviertete-en-orador">Conviertete en orador</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-platzi" href="#" data-bs-toggle="modal" data-bs-target="#modalCompra" >Comprar tickets</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
